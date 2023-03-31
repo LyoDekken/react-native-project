bfe8af68e2113a9ed742019a9c9651fa2ae206b4
LyoDekken <humberto.araripe12@gmail.com>
2023-03-31 02:32:32 -0300

    feat: wrap pages inside navigation

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -6,8 +6,23 @@
 
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { RootStackParamList } from "../../@types/navigationTypes";
+import { NavigationContainer } from "@react-navigation/native";
 
-const Stack = createNativeStackNavigator();
+
+export default function Navigation() {
+  return (
+    <NavigationContainer>
+      <AuthNavigator />
+    </NavigationContainer>
+  );
+}
+
+/**
+ * A root stack navigator is often used for displaying modals on top of all other content.
+ * https://reactnavigation.org/docs/modal
+ */
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 function AuthNavigator() {
   return (
@@ -17,4 +32,3 @@ function AuthNavigator() {
   );
 }
 
-export default AuthNavigator;
